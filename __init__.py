9c1ff04c6f6e82f4ab502f0226a9bb02ca88a2e3
TrebuTrey <trey.pubins@gmail.com>
2023-01-13 13:52:03 -0700

    added notification.logger

--- __init__.py
+++ __init__.py
@@ -4,6 +4,7 @@ from dex import logger as dex_logger
 from emulator import logger as emulator_logger
 from image import logger as image_logger
 from main import logger as main_logger
+from notifications import logger as notification_logger
 from helpers.delay import logger as helpers_delay_logger
 from helpers.log import get_logger
 
@@ -13,4 +14,5 @@ dex_logger = get_logger(dex_logger.name, config.LOG_LEVEL)
 emulator_logger = get_logger(emulator_logger.name, config.LOG_LEVEL)
 image_logger = get_logger(image_logger.name, config.LOG_LEVEL)
 main_logger = get_logger(main_logger.name, config.LOG_LEVEL)
+notification_logger = get_logger(notification_logger.name, config.LOG_LEVEL)
 helpers_delay_logger = get_logger(helpers_delay_logger.name, config.LOG_LEVEL)
