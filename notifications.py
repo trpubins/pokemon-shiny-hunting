0d2b7a8adf4845c292cd8ccb3f227269fa6d142f
Tanner Pubins <t.pubins@icloud.com>
2023-01-12 22:02:22 -0700

    introduce global variables

--- notifications.py
+++ notifications.py
@@ -3,39 +3,38 @@ import email, smtplib, ssl
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
+NAME = "Tyranitar"
+POKEMON = "Gyarados"
 port = 465
 
 sender_email = "pokepy155@gmail.com"
 password = "iieszmlurcusscoa"
 
-name = "Tyranitar"
 receiver_email = "treysauce03@gmail.com"
 
 message = MIMEMultipart("alternative")
-message["Subject"] = "multipart test"
+message["Subject"] = "A Shiny Has Been FOUND!"
 message["From"] = sender_email
 message["To"] = receiver_email
-
-text = """
-    Subject: Hi there
-    
-    This message is sent from Python."""
 
 html = f"""
 <html>
     <body>
-        <p>Hi {name},<br>
-            How are you?<br>
+        <p>Hi {NAME},<br>
+            I hope you're sitting down, because the news of a lifetime is coming upon you.<br>
+        <p>
+        <p>
+            Thanks in no small part to the ingenuity of our development team, we have been able to afford the luxury of carefree
+            shiny hunting. Upon arrival to your gaming system, you will find the battle in progress of a shiny {POKEMON}! Take great care
+            to catch the beast as there are no assurances of another one arriving anytime soon.
         <p>
     <body>
 <html>
 """
 
-part1 = MIMEText(text, "plain")
-part2 = MIMEText(html, "html")
+part = MIMEText(html, "html")
 
-message.attach(part1)
-message.attach(part2)
+message.attach(part)
 
 context = ssl.create_default_context()
 
