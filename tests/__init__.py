ebff043e24240ee0e3d6d212b67d744308ad2bb7
TrebuTrey <trey.pubins@gmail.com>
2023-01-22 02:31:13 -0700

    moved ITEM_LIST for Test_10 to init.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -30,6 +30,12 @@ POKEMON_LIST = [
     { "test_img_num": 15, "name":   "Mr. Mime", "sprite_type": SpriteType.NORMAL },
     { "test_img_num": 16, "name": "Farfetch'd", "sprite_type": SpriteType.NORMAL },
 ]
+ITEM_LIST = [
+    {'name': 'ball', 'item_1': 'masterball', 'item_2': 'greatball', 'item_3': 'pokeball', 'item_4': 'ultraball', 'item_5': None},
+    {'name': 'ball1', 'item_1': 'greatball', 'item_2': None, 'item_3': None, 'item_4': None, 'item_5': None},
+    {'name': 'key', 'item_1': 'squirtbottle', 'item_2': 'redscale', 'item_3': 'basementkey', 'item_4': 'cardkey', 'item_5': 'clearbell'},
+    {'name': 'tm', 'item_1': 'dynamicpunch', 'item_2': 'rollout', 'item_3': 'irontail', 'item_4': 'dragonbreath', 'item_5': 'shadowball'}
+    ]
 
 # check if zipfile requires unzipping
 with ZipFile(os.path.join(TEST_FILES_DIR, "test_images.zip"), 'r') as zip:
