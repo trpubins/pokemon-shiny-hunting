221224087501412894bb13db980005777af8e022
Tanner Pubins <t.pubins@icloud.com>
2023-01-23 09:49:21 -0700

    Define functionality for static encounters from main.

--- emulator.py
+++ emulator.py
@@ -9,6 +9,7 @@ import pyautogui as gui
 
 from config import EMULATOR_NAME, POKEMON_GAME, RETROARCH_APP_FP
 from controller import EmulatorController, press_key
+from pokemon import Pokemon
 from helpers.delay import delay
 from helpers.log import mod_fname
 logger = logging.getLogger(mod_fname(__file__))
@@ -26,8 +27,15 @@ class Emulator():
         self.cont = EmulatorController()
         self.state = EmulatorState()
     
-    def run_game(self):
-        """Run the game inside the emulator."""
+    def continue_pokemon_game(self):
+        """Continue the Pokémon game from last save."""
+        logger.debug("continue game")
+        # TODO - need to implement
+        # assume game has been launched
+        # need to get past load screen and press through Continue
+
+    def launch_game(self):
+        """Launch the game inside the emulator."""
         self.launch()
 
         logger.debug("navigating to game")
@@ -54,6 +62,14 @@ class Emulator():
             gui.write(EMULATOR_NAME)
             gui.press("Enter")
         delay(3)  # ensure sys is fully open & ready to perform next action
+    
+    def find_shiny(self, pokemon: Pokemon, static_enounter: bool = True) -> bool:
+        """Find a shiny Pokémon."""
+        if static_enounter:
+            # TODO - implement functionality using separate module, encounter.py
+            pass
+        # until implemented, always return True
+        return True
 
     def reset(self):
         """Reset the emulator."""
@@ -101,4 +117,4 @@ class EmulatorState():
 
 if __name__ == "__main__":
     em = Emulator()
-    em.run_game()
+    em.launch_game()
