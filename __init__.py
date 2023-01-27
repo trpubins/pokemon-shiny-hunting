2c36640a7e09029b1fbab8fe8393dcf8cf52981d
Tanner Pubins <t.pubins@icloud.com>
2023-01-27 15:24:44 -0700

    Create module to model a pokemon pack.

--- __init__.py
+++ __init__.py
@@ -4,7 +4,9 @@ from dex import logger as dex_logger
 from emulator import logger as emulator_logger
 from image import logger as image_logger
 from main import logger as main_logger
+from menu import logger as menu_logger
 from notifications import logger as notifications_logger
+from pack import logger as pack_logger
 from pokemon import logger as pokemon_logger
 from helpers.delay import logger as helpers_delay_logger
 from helpers.log import get_logger
@@ -15,6 +17,8 @@ dex_logger = get_logger(dex_logger.name, config.LOG_LEVEL)
 emulator_logger = get_logger(emulator_logger.name, config.LOG_LEVEL)
 image_logger = get_logger(image_logger.name, config.LOG_LEVEL)
 main_logger = get_logger(main_logger.name, config.LOG_LEVEL)
+menu_logger = get_logger(menu_logger.name, config.LOG_LEVEL)
 notifications_logger = get_logger(notifications_logger.name, config.LOG_LEVEL)
+pack_logger = get_logger(pack_logger.name, config.LOG_LEVEL)
 pokemon_logger = get_logger(pokemon_logger.name, config.LOG_LEVEL)
 helpers_delay_logger = get_logger(helpers_delay_logger.name, config.LOG_LEVEL)
---
+++ pack.py
@@ -0,0 +1,152 @@
+"""Model a Pokémon pack."""
+
+from enum import Enum
+import logging
+import os
+import platform
+from typing import List, Tuple
+
+from emulator import Emulator
+from image import (
+    determine_pack_items,
+    get_latest_screenshot_fn
+)
+from helpers.delay import delay
+from helpers.log import mod_fname
+logger = logging.getLogger(mod_fname(__file__))
+
+
+MAX_PACK_ITEMS = 5
+"""The max number of items shown in a single pack screenshot."""
+
+
+class Pack():
+    """Describes all the items in a pack."""
+    def __init__(self, emulator: Emulator):
+        all_inventory = collect_pack_inventory(emulator)
+        self.items,self.machines,self.keyitems,self.balls = all_inventory
+
+
+class Inventory():
+    """Generic class to track inventory."""
+    def __init__(self, inventory: List[Tuple[str,int]]):
+        self.inventory = dict()
+        for name,qty in inventory:
+            self.inventory[name] = qty
+
+
+class InventoryNoQty():
+    """Generic class to track inventory without considering quantity."""
+    def __init__(self, inventory: List[Tuple[str,int]]):
+        self.inventory = list()
+        for name,_ in inventory:
+            self.inventory.append(name)
+
+
+class Items(Inventory):
+    """Track inventory of pack Items."""
+    pass
+
+
+class KeyItems(InventoryNoQty):
+    """Track inventory of pack Key Items."""
+    pass
+
+
+class Machines(Inventory):
+    """Track inventory of pack TM/HM."""
+    def __init__(self, inventory: List[Tuple[str,int]]):
+        super().__init__(inventory)
+        self.tm = dict()  # technical machine, TM
+        self.hm = list()  # hidden machine, HM
+        for name,qty in inventory:
+            if qty is not None:
+                self.tm[name] = qty
+            else:
+                self.hm.append(name)
+
+
+class Balls(Inventory):
+    """Track inventory of pack Balls."""
+    pass
+
+
+class BallType(str, Enum):
+    """Enumeration for ball types."""
+    MASTER = "masterball"
+    ULTRA = "ultraball"
+    GREAT = "greatball"
+    POKE = "pokeball"
+    SAFARI = "safariball"
+    FAST = "fastball"
+    LEVEL = "levelball"
+    LURE = "lureball"
+    HEAVY = "heavyball"
+    LOVE = "loveball"
+    FRIEND = "friendball"
+    MOON = "moonball"
+    SPORT = "sportball"
+
+
+def collect_pack_inventory(emulator: Emulator) -> Tuple[Items, Machines, KeyItems, Balls]:
+    """Collect inventory of all items in the pack."""
+    # assume user is in the Pokémon world
+    # assume pause menu has not yet been interacted
+    emulator.press_start(delay_after_press=0.25)
+    emulator.nav_down(presses=2)
+    emulator.press_a(delay_after_press=0.25)
+    
+    # get inventory of each section in the pack
+    items_list = collect_inventory(emulator, get_qty=True)
+    emulator.nav_left(presses=1)
+    machines_list = collect_inventory(emulator, get_qty=True)
+    emulator.nav_left(presses=1)
+    keyitems_list = collect_inventory(emulator, get_qty=False)
+    emulator.nav_left(presses=1)
+    balls_list = collect_inventory(emulator, get_qty=True)
+
+    # back out of the pause menu
+    emulator.press_b(presses=1, delay_after_press=0.5)
+    emulator.press_b(presses=1, delay_after_press=0.25)
+    
+    return (
+        Items(items_list), 
+        Machines(machines_list),
+        KeyItems(keyitems_list),
+        Balls(balls_list)
+    )
+
+
+def collect_inventory(emulator: Emulator, get_qty: bool) -> List[Tuple[str, int]]:
+    """Generic function to collect inventory for any section in the pack."""
+    inventory = []
+    unique_pack_items = [None]  # initialize with None element to kick off while loop
+    emulator.nav_down(presses=MAX_PACK_ITEMS - 1)  # assume cursor starts on unique item
+    while len(inventory) % MAX_PACK_ITEMS == 0 and len(unique_pack_items) > 0:
+        emulator.take_screenshot(delay_after_press=0.25)
+        pack_img_fn = get_latest_screenshot_fn()
+        pack_items = determine_pack_items(pack_img_fn, get_qty=get_qty)
+        os.remove(pack_img_fn)
+        
+        # add only unique items to inventory
+        unique_pack_items = [e for e in pack_items if e not in inventory]
+        inventory += unique_pack_items
+        emulator.nav_down(presses=MAX_PACK_ITEMS)
+        
+        if platform.system() == "Darwin" and len(unique_pack_items) > 0:
+            # wait for screenshot banner to disappear from gui
+            delay(1.5)
+    return inventory
+
+
+if __name__ == "__main__":
+    import __init__
+    em = Emulator()
+    em.launch_game()
+    em.continue_pokemon_game()
+    pack = Pack(em)
+    logger.info(f"balls: {pack.balls.inventory}")
+    logger.info(f"items: {pack.items.inventory}")
+    logger.info(f"tm: {pack.machines.tm}")
+    logger.info(f"hm: {pack.machines.hm}")
+    logger.info(f"key items: {pack.keyitems.inventory}")
