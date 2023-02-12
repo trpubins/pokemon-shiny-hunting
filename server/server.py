5997b1f28eb1921b62fdce45e7224a57fe2ae27d
Tanner Pubins <t.pubins@icloud.com>
2023-02-12 16:25:19 -0700

    Use Queue for multiprocessing communication.
     * Utilize main module run function in separate process
    
     * Determine server availability based on background process state

--- server/server.py
+++ server/server.py
@@ -1,6 +1,7 @@
 """Serves up endpoints to drive the pokemon shiny hunting application."""
 
 import logging
+from multiprocessing import Process, Queue
 # add workspace dir to system path, otherwise cannot import project modules
 import os
 import sys
@@ -8,26 +9,25 @@ proj_root_path = os.path.join(
     os.path.dirname(os.path.realpath(__file__)), os.pardir
 )
 sys.path.append(proj_root_path)
+from typing import Tuple
 
-from fastapi import BackgroundTasks, FastAPI, Response, status
-from starlette.requests import Request
+from fastapi import FastAPI, Response, status
 
 import __init__
+from main import run as go_shiny_hunt
 from emulator import Emulator
 from encounter import StaticEncounter
-from image import get_latest_screenshot_fn
-from notifications import send_notification
 from pokemon import Pokemon
-from helpers.file_mgmt import cdtmp
+from helpers.common import delay
 from helpers.log import mod_fname
 logger = logging.getLogger(mod_fname(__file__))
 
 
 # globals
 app = FastAPI()
-app.emulator: Emulator = None
-app.encounter: StaticEncounter = None
-app.server_available: bool = True
+app.pokemon: Pokemon = None
+app.bkgd_process: Process = None  # prefer process over background task (thread) so that we can easily terminate it
+app.queue: Queue = Queue()  # requried for multiprocess communication
 
 # 4 endpoints:
 #### /            - Provides status on the server (available or busy)
@@ -37,7 +37,7 @@ app.server_available: bool = True
 
 @app.get("/", status_code=status.HTTP_200_OK)
 def status_server():
-    if app.server_available:
+    if is_server_available():
         msg = "Server Available"
     else:
         msg = "Server Busy"
@@ -46,28 +46,30 @@ def status_server():
 
 
 @app.get("/shiny_hunt/{pokemon_name}", status_code=status.HTTP_200_OK)
-async def shiny_hunt(pokemon_name: str, background_tasks: BackgroundTasks, resp: Response):
+async def shiny_hunt(pokemon_name: str, resp: Response):
     
     logger.info(f"received {pokemon_name} as path parameter")
 
-    if not app.server_available:
+    if not is_server_available():
         resp.status_code = status.HTTP_503_SERVICE_UNAVAILABLE
         msg = "Servery Busy"
         logger.info(f"response: {msg}")
         return msg
-    
-    # update server state
-    app.server_available = False
 
     # create objects and assign to state variables
     emulator = Emulator()
     pokemon = Pokemon(pokemon_name)
     encounter = StaticEncounter(emulator, pokemon)
-    app.emulator = emulator
-    app.encounter = encounter
+    app.pokemon = pokemon
 
     # execute hunt in the background (asynchronously)
-    background_tasks.add_task(go_shiny_hunt, emulator, encounter, logger)
+    kwargs = {
+        "emulator": emulator,
+        "encounter": encounter,
+        "queue": app.queue
+    }
+    app.bkgd_process = Process(target=go_shiny_hunt, kwargs=kwargs)
+    app.bkgd_process.start()
     
     # provide response immediately
     msg = f"Started shiny hunt for {encounter.pokemon.name}"
@@ -77,58 +79,44 @@ async def shiny_hunt(pokemon_name: str, background_tasks: BackgroundTasks, resp:
 
 @app.get("/status_hunt", status_code=status.HTTP_200_OK)
 def status_hunt():
-    if app.server_available:
+    if is_server_available():
         msg = "No hunt to status"
     else:
-        if app.encounter.shiny_found:
-            msg = f"Shiny {app.encounter.pokemon.name} found after {app.encounter.n_attempts} attempts"
+        shiny_found, n_attempts = get_app_state()
+        if shiny_found:
+            msg = f"Shiny {app.pokemon.name} found after {n_attempts} attempts"
         else:
-            msg = f"No shiny {app.encounter.pokemon.name} found after {app.encounter.n_attempts} attempts"
+            msg = f"No shiny {app.pokemon.name} found after {n_attempts} attempts"
     logger.info(f"response: {msg}")
     return msg
 
 
 @app.get("/stop_hunt", status_code=status.HTTP_200_OK)
 def stop_hunt():
-    if app.server_available:
+    if is_server_available():
         msg = "No hunt to stop"
     else:
-        kill_emulator_process(app.emulator)
-        msg = f"Stopped hunting {app.encounter.pokemon.name}"
+        logger.warning(f"terminating pid {app.bkgd_process.pid}")
+        app.bkgd_process.terminate()
+        app.bkgd_process = None
+        msg = f"Stopped hunting {app.pokemon.name}"
     logger.info(f"response: {msg}")
     return msg
 
 
-def go_shiny_hunt(emulator: Emulator, encounter: StaticEncounter, logger: logging.Logger):
-    """Try to find a shiny from a static encounter."""
-    emulator.launch_game()
-    try:
-        with cdtmp(sub_dirname="pokemon_shiny_hunting"):
-            shiny_found = encounter.find_shiny(max_attempts=8000)
-            logger.info(f"total number attempts: {encounter.n_attempts}")
-    except KeyboardInterrupt as k:
-        logger.warning("Keyboard interrupt by user")
-        kill_emulator_process(emulator)
-        raise k
-    except Exception as e:
-        logger.error("Exception occurred while shiny hunting")
-        kill_emulator_process(emulator)
-        raise e
-    
-    if shiny_found:
-        emulator.save_state()
-        emulator.fast_fwd_off()
-        emulator.pause_on()
-        attachments = [get_latest_screenshot_fn()]
-    else:
-        kill_emulator_process(emulator)
-        attachments = []
-    send_notification(encounter.pokemon, encounter.n_attempts, shiny_found,
-                      attachments=attachments, send=True)
+def is_server_available() -> bool:
+    """Uses server state to determine if it is available for new request."""
+    return app.bkgd_process is None or not app.bkgd_process.is_alive()
 
 
-def kill_emulator_process(emulator: Emulator):
-    """Kill the process running the emulator and
-    indicate the server is once again available."""
-    emulator.kill_process()
-    app.server_available = True
+def get_app_state() -> Tuple[bool, int]:
+    """Retrieve the state of the shiny hunting application."""
+    logger.info("checking for data in the queue")
+    while app.queue.empty():
+        # wait for queue to populate
+        delay(1, universal=True)
+    app_state = app.queue.get()
+    shiny_found: bool = app_state["shiny_found"]
+    n_attempts: int = app_state["n_attempts"]
+
+    return shiny_found, n_attempts
