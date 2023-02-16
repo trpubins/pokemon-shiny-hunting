aa461309a01320f9d75dfe31056a1eacd105d3ea
Tanner Pubins <t.pubins@icloud.com>
2023-02-15 23:18:12 -0700

    Copy native save files in main module. Perform auto cleanup.

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ from multiprocessing import Queue
 import signal
 import sys
 
+from config import NATIVE_SAVES_DIR, ROM_NAME, RETROARCH_CFG
 from emulator import Emulator
 from encounter import StaticEncounter
 from image import get_latest_screenshot_fn
 from notifications import send_notification
 from pokemon import Pokemon
+from retroarch import cleanup_save_dir, copy_native_save
 from helpers.file_mgmt import cdtmp
 from helpers.log import mod_fname
 logger = logging.getLogger(mod_fname(__file__))
@@ -21,10 +23,24 @@ def run(emulator: Emulator,
         send_email: bool = True,
         queue: Queue = None):
     """Try to find a shiny from a static encounter."""
-    
+    og_rename_files = copy_native_save(pokemon_name=encounter.pokemon.name,
+                                       user_rom_name=ROM_NAME,
+                                       native_saves_dir=NATIVE_SAVES_DIR,
+                                       retroarch_saves_dir=RETROARCH_CFG.savefile_dir)
+
+    def kill_proc_and_cleanup():
+        """Kill the emulator process and clean up
+        the saves directory."""
+        emulator.kill_process()
+        og_rename_rtc_file, og_rename_srm_file = og_rename_files
+        cleanup_save_dir(user_rom_name=ROM_NAME,
+                         retroarch_saves_dir=RETROARCH_CFG.savefile_dir,
+                         renamed_rtc_file=og_rename_rtc_file,
+                         renamed_srm_file=og_rename_srm_file)
+
     def signal_handler(signalnum: int, frame):
         """Kill the emulator and log the received signal."""
-        emulator.kill_process()
+        kill_proc_and_cleanup()
         if signalnum == signal.SIGINT:
             logger.warning("Keyboard interrupt (ctrl + c)")
             sys.exit(1)
@@ -58,7 +74,7 @@ def run(emulator: Emulator,
 
             logger.info(f"total number attempts: {n_attempts}")
     except Exception as e:
-        emulator.kill_process()
+        kill_proc_and_cleanup()
         logger.error("Exception occurred while shiny hunting")
         raise e
     
@@ -68,7 +84,7 @@ def run(emulator: Emulator,
         emulator.pause_on()
         attachments = [get_latest_screenshot_fn()]
     else:
-        emulator.kill_process()
+        kill_proc_and_cleanup()
         attachments = []
     send_notification(encounter.pokemon,
                       n_attempts,
