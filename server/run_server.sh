04184708b61ceaa371e1522c43aa40fa13e8fb78
Tanner Pubins <t.pubins@icloud.com>
2023-02-15 22:02:29 -0700

    Do not run server in separate process. This leads to internal server error.

--- server/run_server.sh
+++ server/run_server.sh
@@ -3,5 +3,4 @@ cd $POKEMON_PROJ_PATH/server && \
     $POKEMON_PROJ_PATH/.venv/bin/uvicorn \
     server:app \
     --host 0.0.0.0 \
-    --port 8000 \
-    &  # tells bash to run this command in a separate process
+    --port 8000
